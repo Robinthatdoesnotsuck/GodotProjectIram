f9c813a45095ba23fa20f650ef1bb82a8b87355a
John Doe <johndoe@example.com>
2022-08-22 22:48:32 -0500

    Added initial setup

---
+++ Player.gd
@@ -0,0 +1,71 @@
+extends KinematicBody
+
+#next change scene and update score
+var moveSpeed:float = 5
+var jumpForce:float = 5
+var gravity:float=12
+
+
+#camlook
+var minLookAngle:float = -90
+var maxlookAngle:float = 90
+var sensitivity:float = 10
+var velocity:Vector3 = Vector3()
+var mouseDelta: Vector2 = Vector2()
+#var scoreUI:RichTextLabel
+
+
+onready var camera :Camera = get_node("Camera")#only when node is initialized
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	
+
+
+
+func _physics_process(delta):#called 60 times per sec
+	velocity.x = 0
+	velocity.z = 0
+	var input:Vector2 = Vector2()
+	if Input.is_action_pressed("move_forward"):
+		input.y+=1
+	if Input.is_action_pressed("move_back"):
+		input.y-=1	
+	if Input.is_action_pressed("move_left"):
+		input.x+=1		
+	if Input.is_action_pressed("move_right"):
+		input.x-=1		
+	input.normalized();
+	
+	var forward = global_transform.basis.z;
+	var right = global_transform.basis.x;
+	
+	var relativeDirection = (forward * input.y + right*input.x);
+	velocity.x = relativeDirection.x * moveSpeed
+	velocity.z = relativeDirection.z * moveSpeed
+	
+	velocity.y -=gravity*delta;#every second we remove delta
+	velocity=  move_and_slide(velocity, Vector3.UP);
+	
+	
+	pass
+	if (Input.is_action_pressed("jump")) and is_on_floor():
+		velocity.y = jumpForce
+		
+		
+
+	
+func _process(delta):#not physics related
+	camera.rotation_degrees.x -= mouseDelta.y*sensitivity*delta
+	
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle,maxlookAngle)
+	#around y axis
+	rotation_degrees.y -= mouseDelta.x*sensitivity*delta
+	
+	#reset mousedelta
+	mouseDelta = Vector2()
+func _input(event):
+	#print("Test")
+	if event is InputEventMouseMotion	:
+		mouseDelta = event.relative
